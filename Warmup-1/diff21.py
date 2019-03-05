7fa9629c098cc48b86af6c2615795da333f8025d
Matt-Gleich <matthewgleich@gmail.com>
2019-03-05 13:16:47 -0500

    Created diff21.py

---
+++ Warmup-1/diff21.py
@@ -0,0 +1,13 @@
+"""
+Problem:
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+"""
+
+
+def diff21(n):
+    if n > 21:
+        ad = (n - 21) * 2
+        return ad
+    else:
+        ad = 21 - n
+        return ad
